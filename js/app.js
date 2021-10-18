43886413ffea3801e8804ea02820c29d22ea3735
Your Name <castillonicolas@gmail.com>
2021-10-18 08:14:24 -0300

    18/10

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $(".pc , .cel").toggle();
     });
     $("#computadoras").click(function(){
-        $(".videojuegos , .cel").toggle();
+        $(".videojuego , .cel").toggle();
     });
     $("#celulares").click(function(){
         $(".pc , .videojuego").toggle();
