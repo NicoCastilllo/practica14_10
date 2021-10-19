3ea6022041805a0a07eb16d56a37f17f01fe5f85
NicoCastillo <castillonicolas1996@gmail.com>
2021-10-19 11:26:15 -0300

    19/10

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
 $(document).ready(function(){
+    let= activoConsola= false
+    let= activoCelulares= false
+    let= activoPc= false
+    
     $("#consolas").click(function(){
-        $(".pc , .cel").toggle();
+      
+        if(activoConsola == false){
+            $(".pc,.cel,.videojuego").show();
+            $(".pc , .cel").toggle();
+            activoConsola=true
+        }else{
+            $(".pc,.cel,.videojuego").show();
+            activoConsola =false
+        }
     });
+    
     $("#computadoras").click(function(){
+      if(activoPc == false){
+        $(".pc,.cel,.videojuego").show();
         $(".videojuego , .cel").toggle();
+        activoPc = true
+    }else{
+        $(".pc,.cel,.videojuego").show();
+        activoPc = false
+    }
     });
+    
     $("#celulares").click(function(){
+        if(activoCelulares == false){
+        $(".pc,.cel,.videojuego").show();
         $(".pc , .videojuego").toggle();
+        activoCelulares = true
+    }else{
+        $(".pc,.cel,.videojuego").show();
+        activoCelulares = false
+    }
+    });
+    
+    $("#reset").click(function(){
+        $(".pc,.cel,.videojuego").show();
     });
 })
